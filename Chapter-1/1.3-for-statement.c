d57d79b7b6f76aee68d637d7f8f7f1fb82b662a0
William Cotton <williamcotton@gmail.com>
2023-11-11 12:03:16 -0600

    adds 1.3 for loop

---
+++ Chapter-1/1.3-for-statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>  
+
+main()
+{
+int curr;
+int start = 0;
+int buffer = 2; 
+int end = 10;
+
+for(curr = start; curr < end; curr = curr + buffer)
+{
+    printf("%3d\n", curr);
+}
+
+
+}
+
